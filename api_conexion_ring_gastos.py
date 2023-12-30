eca8033566c10f11292d041062ef0bfc9b39eb53
Tonny <corner.store59@gmail.com>
2023-12-30 17:17:02 -0600

    [ADD] Se agrega el script para la comunicacion a la api de ring de gastos y almacenar la informacion en la tabla de gastos en la base de datos de procesos

---
+++ api_conexion_ring_gastos.py
@@ -0,0 +1,51 @@
+import requests
+import psycopg2
+
+
+def process_expenses_date(_expenses):
+    db1_conn = psycopg2.connect(
+        dbname='procesos_01',
+        user='postgres',
+        password='1234',
+        host='localhost',
+        port='5432'
+    )
+    db1_cursor = db1_conn.cursor()
+    for expense in _expenses:
+        if expense['Status'] == 1:
+            state = 'Aprobado'
+            db1_cursor.execute(f'INSERT INTO mp_gastos (id, concepto, monto, empleado_id, estado) VALUES'
+                               f' ({expense["Id"]}, ' + "'" + expense["Category"] + "'" + f', {expense["Total"]}, {expense["UserId"]}, ' + "'" + state + "'" + f');')
+            db1_conn.commit()
+
+# URL base de la API
+url_base = "https://api.rindegastos.com/v1"
+
+# Ruta específica dentro de la API
+ruta_especifica = "/getExpenses?Currency=CLP"
+
+# URL completa para la solicitud
+url_completa = url_base + ruta_especifica
+
+# Clave de autorización
+clave_autorizacion = "test"
+
+# Encabezados de la solicitud con la clave de autorización
+headers = {
+    "Authorization": f"Bearer {clave_autorizacion}"
+}
+
+# Realizar la solicitud GET sin verificar SSL
+respuesta = requests.get(url_completa, headers=headers)
+
+# Verificar el código de estado de la respuesta
+if respuesta.status_code == 200:
+    # La solicitud fue exitosa, puedes trabajar con la respuesta en formato JSON
+    datos_json = respuesta.json()
+    process_expenses_date(datos_json['Expenses'])
+    for i in range(2, (datos_json['Records']['Pages'] + 1)):
+        request = requests.get(url_completa + f'&Page={i}', headers=headers)
+        json_result = request.json()
+        process_expenses_date(json_result['Expenses'])
+else:
+    print(f"La solicitud falló con el código de estado: {respuesta.status_code}")
