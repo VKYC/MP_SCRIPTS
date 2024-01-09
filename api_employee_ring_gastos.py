884770c239f921e1e277f1d2a3f9d4a4c091b70d
Tonny <corner.store59@gmail.com>
2024-01-09 12:37:23 -0600

    [FIX] Update records employee

--- api_employee_ring_gastos.py
+++ api_employee_ring_gastos.py
@@ -12,18 +12,20 @@ def process_expenses_date(_expenses):
     )
     db1_cursor = db1_conn.cursor()
     for expense in _expenses:
-        employee_id = int(expense['EmployeeNumber']) if expense['EmployeeNumber'] else "-1"
         try:
+            employee_id = int(expense['EmployeeNumber']) if expense['EmployeeNumber'] else "-1"
             db1_cursor.execute(
                 f"INSERT INTO mp_usuarios (empleado_id, usuario_ext_id, email, identificacion, posicion, fecha_creacion) VALUES "
                 f"({employee_id}, {expense['Id']}, '{expense['Email']}', '{expense['Identification']}', '{expense['Position']}', '{expense['CreatedAt']}');")
 
         except psycopg2.IntegrityError as e:
             # Si se produce un error de integridad, asumimos que el ID ya existe, entonces actualizamos en lugar de insertar
+            employee_id = int(expense['EmployeeNumber']) if expense['EmployeeNumber'] else "-1"
             db1_conn.rollback()
             db1_cursor.execute(f"UPDATE mp_usuarios SET empleado_id = {employee_id},"
                                f" email = '{expense['Email']}', identificacion = '{expense['Identification']}', "
-                               f"posicion = '{expense['Position']}', fecha_creacion = '{expense['CreatedAt']}';")
+                               f"posicion = '{expense['Position']}', fecha_creacion = '{expense['CreatedAt']}'"
+                               f" WHERE usuario_ext_id = '{expense['Id']}' ;")
 
         db1_conn.commit()
     db1_conn.close()
